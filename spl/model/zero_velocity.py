7764ebbb7e1c0053e5f9bb6a3b5e010d6182766d
eaksan <eaksan@inf.ethz.ch>
2019-09-30 21:15:58 +0200

    Cleaning BaseModel and implementing ZeroVelocity model.

--- spl/model/zero_velocity.py
+++ spl/model/zero_velocity.py
@@ -1,7 +1,7 @@
 import time
 import tensorflow as tf
 
-from base_model import BaseModel
+from model.base_model import BaseModel
 
 
 class ASimpleYetEffectiveBaseline(BaseModel):
@@ -13,36 +13,53 @@ class ASimpleYetEffectiveBaseline(BaseModel):
         super(ASimpleYetEffectiveBaseline, self).__init__(config, data_pl, mode, reuse, **kwargs)
 
         # Dummy variable
-        self._dummy = tf.Variable(0.0, name="imadummy")
+        self._dummy = tf.Variable(0.0, name="dummy_variable")
         
         # Extract the seed and target inputs.
         with tf.name_scope("inputs"):
-            self.encoder_inputs = self.data_inputs[:, 0:self.source_seq_len - 1]
-            self.decoder_inputs = self.data_inputs[:, self.source_seq_len - 1:-1]
-            self.decoder_outputs = self.data_inputs[:, self.source_seq_len:]
-    
-            enc_in = tf.reshape(tf.transpose(self.encoder_inputs, [1, 0, 2]), [-1, self.input_size])
-            dec_in = tf.reshape(tf.transpose(self.decoder_inputs, [1, 0, 2]), [-1, self.input_size])
-            dec_out = tf.reshape(tf.transpose(self.decoder_outputs, [1, 0, 2]), [-1, self.input_size])
-    
-            self.enc_in = tf.split(enc_in, self.source_seq_len - 1, axis=0)
-            self.dec_in = tf.split(dec_in, self.target_seq_len, axis=0)
-            self.dec_out = tf.split(dec_out, self.target_seq_len, axis=0)
-            self.prediction_inputs = self.decoder_inputs
-            self.prediction_targets = self.decoder_outputs
+            self.prediction_inputs = self.data_inputs[:, self.source_seq_len - 1:-1]
+            self.prediction_targets = self.data_inputs[:, self.source_seq_len:]
 
     def build_network(self):
         # Don't do anything, just repeat the last known pose.
-        last_known_pose = self.decoder_inputs[:, 0:1]
-        self.outputs_mu = tf.tile(last_known_pose, [1, self.target_seq_len, 1])
-        self.outputs = self.outputs_mu
+        last_known_pose = self.prediction_inputs[:, 0:1]
+        self.outputs = tf.tile(last_known_pose, [1, self.target_seq_len, 1])
 
     def build_loss(self):
+        # Build a loss operation so that training script doesn't complain.
         d = self._dummy - self._dummy
         self.loss = tf.reduce_mean(tf.reduce_sum(d*d))
+        
+    def summary_routines(self):
+        # Build a summary operation so that training script doesn't complain.
+        tf.summary.scalar(self.mode+"/loss", self.loss, collections=[self.mode+"/model_summary"])
+        self.summary_update = tf.summary.merge_all(self.mode + "/model_summary")
+    
+    def step(self, session):
+        output_feed = [self.loss,
+                       self.summary_update,
+                       self.outputs]
+        outputs = session.run(output_feed)
+        return outputs[0], outputs[1], outputs[2]
+    
+    def sampled_step(self, session):
+        assert self.is_eval, "Only works in sampling mode."
+        prediction, targets, seed_sequence, data_id = session.run([self.outputs,
+                                                                   self.prediction_targets,
+                                                                   self.data_inputs[:, :self.source_seq_len],
+                                                                   self.data_ids])
+        return prediction, targets, seed_sequence, data_id
 
     @classmethod
     def get_model_config(cls, args):
+        """Given command-line arguments, creates the configuration dictionary.
+
+        It is later passed to the models and stored in the disk.
+        Args:
+            args: command-line argument object.
+        Returns:
+            experiment configuration (dict), experiment name (str)
+        """
         config = dict()
         config['seed'] = args.seed
         config['model_type'] = args.model_type
@@ -54,7 +71,7 @@ class ASimpleYetEffectiveBaseline(BaseModel):
         config['source_seq_len'] = args.seq_length_in
         config['target_seq_len'] = args.seq_length_out
     
-        config['num_epochs'] = args.num_epochs
+        config['num_epochs'] = 0
         config['print_frequency'] = args.print_frequency
         config['test_frequency'] = args.test_frequency
     
