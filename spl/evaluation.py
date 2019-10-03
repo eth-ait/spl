de67ecf675f68b34184532d1f91de0a44105e500
eaksan <eaksan@inf.ethz.ch>
2019-10-03 14:55:21 +0200

    Enabling visualization with SMPL model.

--- spl/evaluation.py
+++ spl/evaluation.py
@@ -139,23 +139,26 @@ def evaluate(session, test_model, test_data, args, eval_dir, use_h36m):
     test_metrics, eval_result = evaluate_model(test_model, test_iter, metrics_engine)
     print("Test \t {}".format(metrics_engine.get_summary_string(test_metrics)))
     
-    if args.visualize:
+    if args.visualize_smpl or args.visualize:
         # visualize some random samples stored in `eval_result` which is a dict id -> (prediction, seed, target)
         video_dir = eval_dir if args.save_video else None
         frames_dir = eval_dir if args.save_frames else None
         visualizer = Visualizer(fk_engine, video_dir, frames_dir, rep=representation)
         n_samples_viz = 30  # TODO change
-        selected_idx = [5]  # [5, 6, 7, 19]  # [0, 1, 2, 5, 6, 7, 9, 19, 24, 27] [24, 27]  # for the dynamic split
+        selected_idx = [5, 6, 7]  # [5, 6, 7, 19]  # [0, 1, 2, 5, 6, 7, 9, 19, 24, 27] [24, 27]  # for the dynamic split
         rng = np.random.RandomState(42)
         idx = rng.randint(0, len(eval_result), size=n_samples_viz)
     
         sample_keys = [list(sorted(eval_result.keys()))[i] for i in idx]
         for i, k in enumerate(sample_keys):
             if i in selected_idx:
-                # visualizer.visualize_dense_smpl(np.concatenate([eval_result[k][2], eval_result[k][1]], axis=0),
-                #                                 k+"_i{}".format(i))
-                visualizer.visualize(eval_result[k][2], eval_result[k][0], eval_result[k][1],
-                                     title=k + "_i{}".format(i))
+                print("Visualizing sample " + k)
+                if args.visualize:
+                    visualizer.visualize(eval_result[k][2], eval_result[k][0], eval_result[k][1],
+                                         title=k + "_i{}".format(i))
+                else:
+                    visualizer.visualize_dense_smpl(np.concatenate([eval_result[k][2], eval_result[k][1]], axis=0),
+                                                    k + "_i{}".format(i))
 
 
 if __name__ == '__main__':
@@ -179,7 +182,10 @@ if __name__ == '__main__':
     parser.add_argument('--seq_length_out', required=False, type=int, help="Target sequence length")
     parser.add_argument('--batch_size', required=False, default=64, type=int, help="Batch size")
     
-    parser.add_argument('--visualize', required=False, action="store_true", help="Visualize model predictions.")
+    parser.add_argument('--visualize', required=False, action="store_true",
+                        help="Visualize ground-truth and predictions side-by-side by using human skeleton.")
+    parser.add_argument('--visualize_smpl', required=False, action="store_true",
+                        help="Visualize only predictions by using smpl mesh.")
     parser.add_argument('--save_video', required=False, action="store_true",
                         help="Save the model predictions to mp4 videos in the experiments folder.")
     parser.add_argument('--save_frames', required=False, action="store_true",
